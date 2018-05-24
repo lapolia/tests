9d7510d25894feea43d743da77ed86067ede1865
Admin <Admin@DESKTOP-INJ064T>
2018-05-24 11:48:57 +0700

    111

--- ClassLibrary1/ClassLibrary1/Class1.cs
+++ ClassLibrary1/ClassLibrary1/Class1.cs
@@ -12,57 +12,54 @@ namespace ClassLibrary1
     [TestFixture]
     public class proga_test
     {
-        
+        IWebDriver WebDriver = new ChromeDriver();
 
         [TestCase]
         public void Title()
         {
-            IWebDriver WebDriver = new ChromeDriver();
-
             WebDriver.Url = "https://adventuretime.ru/";
             Assert.AreEqual("Время приключений - Adventure Time смотреть онлайн", WebDriver.Title);
-            
         }
+
         [TestCase]
         public void Obj()
         {
-            IWebDriver WebDriver = new ChromeDriver();
-
+            
             WebDriver.Url = "https://adventuretime.ru/";
             IWebElement element = WebDriver.FindElement(By.XPath("//*[@id='header']/div[2]/div/div[2]/img"));
             bool status = element.Displayed;
         }
+
+
         [TestCase]
         public void Link()
         {
-            IWebDriver WebDriver = new ChromeDriver();
-
             WebDriver.Url = "https://adventuretime.ru/";
             IWebElement element = WebDriver.FindElement(By.XPath("/html/body/div[2]/div/div/div/ul/li[3]/a"));
             element.Click();
-            
-
         }
+
+        IWebDriver WebDriver1 = new ChromeDriver();
 
         [TestCase]
         public void Text()
         {
-            IWebDriver WebDriver = new ChromeDriver();
+            
 
-            WebDriver.Url = "https://adventuretime.ru/";
+            WebDriver1.Url = "https://www.sibsutis.ru/";
 
            // IWebElement div = WebDriver.FindElement(By.XPath("//*[@id=\"ya - site - form0\"]/div/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr/td[1]/div"));
 
            // div.Click();
 
-            IWebElement search = WebDriver.FindElement(By.XPath("//*[@id=\"ya - site - form0\"]/div/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr/td[1]/div/input"));
+            IWebElement search = WebDriver1.FindElement(By.XPath("//*[@id=\"qqq\"]"));
             search.SendKeys("пупырка рулит");
 
-           IWebElement button = WebDriver.FindElement(By.XPath("//*[@id='ya - site - form0']/div/table/tbody/tr/td/table/tbody/tr[1]/td/table/tbody/tr/td[2]/input"));
+           IWebElement button = WebDriver1.FindElement(By.XPath("//*[@id=\"layout\"]/div[2]/div[2]/div[2]/div[3]/form/input[2]"));
             
             button.Click();
+           
 
-            WebDriver.Quit();
         }
 
 
@@ -70,15 +67,9 @@ namespace ClassLibrary1
         [TearDown]
         public void testEnd()
         {
-            
+            WebDriver.Quit();
+            WebDriver1.Quit();
         }
-
-        
-       
-
-
-
-
 
         
     }
